33f0e92a9bef028f91afc313d5d03eb07fd49796
Claire Tsai <claire_tsai@horacemann.org>
2024-10-23 14:07:25 -0400

    claire

---
+++ filetwo.js
@@ -0,0 +1,17 @@
+
+var startButton = document.getElementById("start");
+
+startButton.addEventListener('click', () => {
+    
+})
+
+
+
+function randomValue(min, max){
+    return Math.random() * (max-min) + min; }
+
+Math.floor(Math.random());
+
+
+
+
